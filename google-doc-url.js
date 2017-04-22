2b4e7a12b7e4878d1f4ff9962f9e366a38dff1b7
kuzuha-fx <kuzuha.fx@outlook.com>
2017-04-22 15:36:06 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gq9qWQFXm0FVrQn8hF3nui_ghk1N9wPQCx6ZXcegytM/edit#gid=745832354';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gq9qWQFXm0FVrQn8hF3nui_ghk1N9wPQCx6ZXcegytM/edit#gid=0';
