fe185ead666331bd1b5fa188404f76e0437e0181
lkerith <lkerith@gmail.com>
2017-05-11 13:12:27 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1YVzwU-mLCRP_vjvyQxGl56IlaG-dEOziXXumaSpVYbE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/157Soyh2-ancZRAMynwwkpImSdJ3AZGho2YVn05r8MMo/edit#gid=0';
